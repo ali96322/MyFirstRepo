c7c3b5348809b41b4c62ae1741dbecf7ac0bd5c3
patanbuzz <patanbuzz@gmail.com>
2019-08-19 23:39:51 +0530

    search and title features code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,8 +2,15 @@ package com.qa.pages;
 
 public class LoginPage
 {
+
 	public void loginPage()
 	{
 		System.out.println("loginpage");
+	}
+	
+	//again adding method
+	public void loginSet()
+	{
+		System.out.println("loginSet");
 	}
 }
---
+++ src/main/java/com/qa/pages/TitlePage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class TitlePage
+{
+	public void titlePage()
+	{
+		System.out.println("titlePage");
+	}
+}
